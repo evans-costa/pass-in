269617db91e3e482bc852063be805578f63a4f73
Evandro Costa <evandro.souzac@gmail.com>
2024-04-02 15:03:52 -0300

    Create route to get an event by id

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,7 @@ import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod
 
 import { createEvent } from "./routes/create-event";
 import { registerForEvent } from "./routes/register-for-event";
+import { getEvent } from "./routes/get-event";
 
 export const app = fastify();
 
@@ -11,6 +12,7 @@ app.setSerializerCompiler(serializerCompiler);
 
 app.register(createEvent);
 app.register(registerForEvent);
+app.register(getEvent);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("Server running on port 3333");
