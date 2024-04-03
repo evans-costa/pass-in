1ef33d7b4212e85e0775d39a6a495b890e8c2370
Evandro Costa <evandro.souzac@gmail.com>
2024-04-03 14:46:50 -0300

    Create route to get all attendees by event

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,7 +3,9 @@ import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod
 
 import { createEvent } from "./routes/create-event";
 import { registerForEvent } from "./routes/register-for-event";
+import { checkIn } from "./routes/check-ins";
 import { getEvent } from "./routes/get-event";
+import { getEventAttendees } from "./routes/get-event-attendees";
 import { getAttendeeBadge } from "./routes/get-attendee-badge";
 
 export const app = fastify();
@@ -13,7 +15,10 @@ app.setSerializerCompiler(serializerCompiler);
 
 app.register(createEvent);
 app.register(registerForEvent);
+app.register(checkIn);
+
 app.register(getEvent);
+app.register(getEventAttendees);
 app.register(getAttendeeBadge);
 
 app.listen({ port: 3333 }).then(() => {
