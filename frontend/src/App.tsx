11f97b25e718926e1dc893fd963b21f9c39c9b5f
Evandro Costa <evandro.souzac@gmail.com>
2024-04-04 18:55:16 -0300

    Fix import from Attendee List

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
-import { AttendeeList } from "./components/Attendee-List";
+import { AttendeeList } from "./components/AttendeeList";
 import { Header } from "./components/Header";
 
 export function App() {
   return (
-    <div className='max-w-[1216px] mx-auto py-5 flex flex-col gap-5'>
+    <div className="max-w-[1216px] mx-auto py-5 flex flex-col gap-5">
       <Header />
       <AttendeeList />
     </div>
