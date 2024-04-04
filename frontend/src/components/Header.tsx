5adade1539f3edb707a40f3bb701bf2756c430ff
Evandro Costa <evandro.souzac@gmail.com>
2024-04-04 18:54:14 -0300

    Put `NavLink` component on `Header`

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,16 +1,13 @@
 import icon from "../assets/icon.svg";
+import { NavLink } from "./NavLink";
 
 export function Header() {
   return (
-    <div className='flex items-center gap-5'>
+    <div className="flex items-center gap-5">
       <img src={icon} />
-      <nav className='flex items-center gap-5'>
-        <a className='font-medium text-sm text-zinc-300' href=''>
-          Eventos
-        </a>
-        <a className='font-medium text-sm' href=''>
-          Participantes
-        </a>
+      <nav className="flex items-center gap-5">
+        <NavLink href="/events">Eventos</NavLink>
+        <NavLink href="/attendees">Partipantes</NavLink>
       </nav>
     </div>
   );
