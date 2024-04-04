3fbe365ae45be7fbc36ffd3c0c359d91371497c8
Evandro Costa <evandro.souzac@gmail.com>
2024-04-03 22:13:00 -0300

    Create seed database function

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,22 +1,60 @@
 import { prisma } from "../src/lib/prisma";
+import { Prisma } from "@prisma/client";
+import { faker } from "@faker-js/faker";
+import dayjs from "dayjs";
 
 async function seed() {
   console.log("> Seeding database...");
 
+  const eventId = "bfa619f2-e367-4190-8e12-e19b19a5ab0c";
+
+  await prisma.event.deleteMany();
+
   await prisma.event.create({
     data: {
-      id: "bfa619f2-e367-4190-8e12-e19b19a5ab0c",
+      id: eventId,
       title: "Evento de Tecnologia",
       slug: "evento-de-tecnologia",
       details: "Evento de tecnologia para programadores",
       maximumAttendees: 120,
     },
   });
+
+  const attendeesToInsert: Prisma.AttendeeUncheckedCreateInput[] = [];
+
+  for (let i = 1; i <= 120; i++) {
+    attendeesToInsert.push({
+      id: 10000 + i,
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      createdAt: faker.date.recent({ days: 30, refDate: dayjs().subtract(8, "days").toDate() }),
+      eventId,
+      ticketId: faker.string.nanoid(10),
+      checkIn: faker.helpers.arrayElement<
+        Prisma.CheckInUncheckedCreateNestedOneWithoutTicketInput | undefined
+      >([
+        undefined,
+        {
+          create: {
+            createdAt: faker.date.recent({ days: 7 }),
+          },
+        },
+      ]),
+    });
+  }
+
+  await Promise.all(
+    attendeesToInsert.map((data) => {
+      return prisma.attendee.create({
+        data,
+      });
+    })
+  );
 }
 
 seed()
   .then(async () => {
-    console.log("Database seeded");
+    console.log("> Database seeded");
     await prisma.$disconnect();
   })
   .catch(async (error) => {
